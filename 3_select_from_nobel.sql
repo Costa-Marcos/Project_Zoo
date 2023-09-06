7c6eca5818e02b8e28b01dcf84cd03992fa7f084
Marcos Costa <mvpc30@protonmail.ch>
2023-09-06 16:19:51 -0300

    select from nobel updated, select within select added

--- 3_select_from_nobel.sql
+++ 3_select_from_nobel.sql
@@ -53,3 +53,18 @@ SELECT yr, subject, winner FROM nobel WHERE (subject = 'Medicine' AND yr < 1910)
 
 SELECT * FROM nobel WHERE winner = 'Peter Grünberg';
 
+-- 12. Apostrophe
+-- Find all details of the prize won by EUGENE O'NEILL
+
+SELECT * FROM nobel WHERE winner = 'Eugene O''Neill';
+
+-- 13. Knights of the realm
+-- Knights in order. List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
+
+SELECT winner, yr, subject FROM nobel WHERE winner LIKE 'Sir%' ORDER BY yr DESC, winner;
+
+
+--14. Chemistry and Physics last
+-- Show the 1984 winners and subject ordered by subject and winner name; but list chemistry and physics last.
+
+SELECT winner, subject FROM nobel WHERE yr = 1984 ORDER BY subject IN ('Chemistry', 'Physics'), subject, winner;
